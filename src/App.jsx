d784115c9f6893f3bdbc4a34f2a897e060effacb
amanVar06 <98205162+amanVar06@users.noreply.github.com>
2022-11-25 10:08:37 +0530

    Completed Initial Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
-import Navbar from "./components/Navbar";
-import SearchFeed from "./components/SearchFeed";
-import VideoDetail from "./components/VideoDetail";
-import ChannelDetail from "./components/ChannelDetail";
-import SearchFeed from "./components/SearchFeed";
+
+import {
+  Navbar,
+  Feed,
+  SearchFeed,
+  VideoDetail,
+  ChannelDetail,
+} from "./components";
 
 const App = () => {
   return (
@@ -12,7 +15,7 @@ const App = () => {
       <Box sx={{ backgroundColor: "#000" }}>
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<SearchFeed />} />
+          <Route path="/" exact element={<Feed />} />
           <Route path="/video/:id" exact element={<VideoDetail />} />
           <Route path="/channel/:id" exact element={<ChannelDetail />} />
           <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
